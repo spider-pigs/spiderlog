b7b0e174c9bef0f022771ae65d1a22da0dec69e8
Patrik Palmér <patrik.palmer@gmail.com>
2019-07-05 09:25:12 +0200

    Add package docs

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package spiderlog implements a simple StackDriver logging package for writing
+// logs. This package wraps the StackDriver client from
+// https://godoc.org/cloud.google.com/go/logging.
+package spiderlog
